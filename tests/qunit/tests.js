44b417c935dced926765bdf5ba59f246ecf3f2c1
Sergej Kurakin <sergej@kurakin.info>
2011-06-19 23:44:07 +0300

    Initialization with Custom Classes Tests

--- tests/qunit/tests.js
+++ tests/qunit/tests.js
@@ -82,5 +82,51 @@ $(function(){
 
 		equal(anyNonHidden, false, 'Other blocks not hidden');
 	});
+	
+	test('Custom Initialization', function(){
+		expect(7);
+
+		$('#my-nav-1').semanticTabs({
+			'tabsCSSClass': 'my-super-tabs',
+			'tabsNavSuffix': '-van',
+			'tabsNavActiveSuffix': '-ruc'
+		});
+		
+		equal($('#my-nav-1').hasClass('my-super-tabs'), true, 'No class .my-super-tabson main object');
+
+		equal($('#my-nav-1 ul.my-super-tabs-van').length, 1, 'Navigation UL block was not found');
+
+		equal($('#my-nav-1 ul.my-super-tabs-van li').length, 3, 'Navigation UL block\'s LI was not found');
+
+		equal($('#my-nav-1 ul.my-super-tabs-van li:first').hasClass('my-super-tabs-ruc'), true, 'First tab in Navigation is not open');
+
+		var lis = $('#my-nav-1 ul.my-super-tabs-van li');
+		var anyHasClass = false;
+		$.each(lis, function(i, obj){
+			if (i > 0) {
+				if ($(obj).hasClass('my-super-tabs-ruc')) {
+					anyHasClass = true;
+				}
+			}
+		});
+
+		equal(anyHasClass, false, 'Other Navigation is open');
+
+		equal($('#my-nav-1 dd:first').css('display'), 'block', 'First tab in Content is not open');
+
+		var dds = $('#my-nav-1 dd');
+		var anyNonHidden = false;
+		$.each(dds, function(i, obj){
+			console.log(i);
+			if (i > 0) {
+				if ($(obj).css('display') !== 'none') {
+					anyNonHidden = true;
+				}
+			}
+		});
+
+		equal(anyNonHidden, false, 'Other blocks not hidden');
+
+	});
 
 });
